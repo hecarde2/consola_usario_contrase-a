256689a9eaf7cc8fe8cc967c832293950fa30e8c
Hector Carvajal de los reyes <hectordico@hotmail.com>
2026-03-03 09:39:35 -0500

    tener correo un @ y despues del @ un .

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ if len(contrasena) >= 8 and \
  any(letra.isupper for letra in contrasena) and \
  any(letra.isdigit for letra in contrasena) and \
  any(letra in "@$-_*+" for letra in contrasena) and\
-   any(letra in "@." for letra in email):
+    "@" in email and \
+   "." in email.split("@")[1]:
     print("usuario creado")
  
 else:
@@ -16,4 +17,5 @@ else:
     print("- Al menos una mayúscula")
     print("- Al menos un número")
     print("- Al menos un carácter especial (@, *, +)")
+    print("Y el correo debe tener un punto después del @")
 
